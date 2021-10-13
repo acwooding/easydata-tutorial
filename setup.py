9c972505302f82d594964788677c654de6fae66c
acwooding <amy@wooding.org>
2021-10-13 09:20:09 -1000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='2.0.0',
+    description='''Tutorial on how to use easydata, first prepared for the Pydata Global 2021 conference.''',
+    author='Amy Wooding',
+    license='MIT',
+)
