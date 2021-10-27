d65d906b3f288ccc513b6f77a81d3fc79cc04431
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2021-10-27 21:51:56 +0000

    Auto-create dir where output notebooks are written

--- quest/data_challenge.py
+++ quest/data_challenge.py
@@ -11,10 +11,12 @@ ds = Dataset.load('penguins-scaled')
 assert len(ds.data) == 334
 
 # Test that the notebook runs to completion
+path_test_runs = paths["notebook_path"] / "test-runs"
+path_test_runs.mkdir(parents=True, exist_ok=True)
 run_notebook(notebook_name="04-Data-Challenge.ipynb",
              notebook_path=paths['notebook_path'],
-             output_notebook_name="04-Testing-Challenge-Test-Run.ipynb",
-             output_notebook_path=paths['notebook_path'] / "test-runs")
+             output_notebook_name="04-Data-Challenge-Test-Run.ipynb",
+             output_notebook_path=path_test_runs)
 
 print("""\n
 ***************************
