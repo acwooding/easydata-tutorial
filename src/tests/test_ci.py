740f964e364a09b3edb52d1394d099220a7f9a20
acwooding <amy@wooding.org>
2021-10-29 00:48:39 -1000

    name typo

--- src/tests/test_ci.py
+++ src/tests/test_ci.py
@@ -29,9 +29,9 @@ class TestDatasetsSmall(unittest.TestCase):
         ds = Dataset.load('penguins-scaled')
 
     def test_notebook_05(self):
-        run_notebook(notebook_name="05-Test-Challenge.ipynb",
+        run_notebook(notebook_name="05-Testing-Challenge.ipynb",
              notebook_path=paths['notebook_path'],
-             output_notebook_name="05-Test-Challenge-Test-Run.ipynb",
+             output_notebook_name="05-Testing-Challenge-Test-Run.ipynb",
              output_notebook_path=notebook_test_path)
 
     def test_notebook_02(self):
