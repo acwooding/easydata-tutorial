602824f311e36574d5816110d32f629b12619f27
acwooding <amy@wooding.org>
2021-10-29 14:21:05 -1000

    add a final message

---
+++ quest/complete_challenge.py
@@ -0,0 +1,13 @@
+print("""\n
+******************************************************
+
+Congratulations! You have completed the
+
+Easydata Quest from Reproducibility.
+
+Thanks for coming along on the journey. We've enjoyed
+having you here. Aloha and Mahalo.
+
+******************************************************
+
+""")
